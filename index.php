87c23ec65b24bb78f7f58d843b868a62430687a2
mepley <mepley@breccia>
2019-08-13 10:55:24 -0400

    Added convenience links to main page

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
 <a href="https://github.com/michaelepley/phpmysqldemo"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/121cd7cbdc3e4855075ea8b558508b91ac463ac2/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_green_007200.png"></a>
 <div align="center" width="80%">
 <h1>Openshift PHP+MySQL </h1>
+<p>Other pages: <a href="?refresh=2">continuous refresh (2s)</a>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<a href="load/primality.php/?number=10000">Load with Primality Tests (CPU)</a>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<a href="load/request.php/?t=10000">Load with Recursive Requests Tests (I/O)</a></p>
 <?php
 // <!-- Automatically refresh page every few seconds, if the refresh request parameter is provided -->  
 if (isset($_GET["refresh"])) {
